a52a921e47858ecf96d85095d0e1f1a023379f89
YanMN50 <yan.matos@ba.estudante.senai.br>
2024-10-24 19:39:58 +0000

    yan

--- Biblioteca.py
+++ Biblioteca.py
@@ -3,4 +3,5 @@ from sqlalchemy import create_engine, Column, String, Integer, Float
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dataclasses import dataclass
 
-db = create_
+db = create_engine("sqlite:///meubanco.db")
+
