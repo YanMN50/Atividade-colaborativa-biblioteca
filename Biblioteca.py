0b3f65328eb90332f54486fcf49077c35aedf69e
YanMN50 <yan.matos@ba.estudante.senai.br>
2024-10-24 19:36:38 +0000

    yan

--- Biblioteca.py
+++ Biblioteca.py
@@ -2,3 +2,5 @@ import os
 from sqlalchemy import create_engine, Column, String, Integer, Float
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dataclasses import dataclass
+
+db = create_
