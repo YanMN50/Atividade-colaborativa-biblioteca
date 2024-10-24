cd32494d35162e42c908e00287d544452d7d6d52
YanMN50 <yan.matos@ba.estudante.senai.br>
2024-10-24 19:53:44 +0000

    yan banco de dados

--- Biblioteca.py
+++ Biblioteca.py
@@ -5,3 +5,28 @@ from dataclasses import dataclass
 
 db = create_engine("sqlite:///meubanco.db")
 
+Session = sessionmaker(bind=db)
+session = Session()
+
+
+Base = declarative_base()
+
+class Cliente(Base):
+    __tablename__ = "clientes"
+
+    matricula = Column(Integer, primary_key=True)
+    nome = Column(String)
+    email = Column(String)
+    senha = Column(String)
+
+    def __init__(self, matricula:int, nome:str, email:str, senha:str):
+        self.matricula = matricula
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+
+Base.metadata.create_all(bind=db)
+
+os.system("cls || clear")
+
